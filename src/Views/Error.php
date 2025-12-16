94e16ea923d649f7104362f2d59b6728ffac43c4
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-12-16 21:27:56 +0000

    toggle error details via env

--- src/Views/Error.php
+++ src/Views/Error.php
@@ -1,5 +1,10 @@
-<h2>Error</h2>
-<?php if ($error ?? false): ?>
-  <div><b>Details</b></div>
-  <p><code><?= $error ?></code></p>
+<?php if (($error ?? false) && getenv('ENV') == 'dev'): ?>
+  <h2>Error</h2>
+  <pre><code><?= $error ?></code></pre>
+<?php else: ?>
+  <div align="center">
+    <h2>Uh oh!</h2>
+    <p>An unexpected error has ocurred during that action</p>
+    <button onclick="history.back()"><i class="bi bi-arrow-left"></i> Go Back</button>
+  </div>
 <?php endif ?>
