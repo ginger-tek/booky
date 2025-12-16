fb6047199cf7bbfb290f33e7d4065e50682e4d92
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-12-16 21:23:54 +0000

    password validation

--- src/Views/Signup.php
+++ src/Views/Signup.php
@@ -5,11 +5,20 @@
     <label>Username
       <input name="username" type="text" autocapitalize="off" required>
     </label>
+    <article class="alert info">Password requirements:
+      <ul style="color:inherit" class="bottom-clear">
+        <li>12 or more characters long
+        <li>Lowercase characters</li>
+        <li>Uppercase characters</li>
+        <li>Numbers</li>
+        <li>Special characters</li>
+      </ul>
+    </article>
     <label>Password <span class="toggle-password" style="cursor:pointer"><i class="bi bi-eye-slash"></i></span>
-      <input name="password" type="password" required>
+      <input name="password" type="password" minlength="12" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{12,}$" required>
     </label>
     <label>Confirm Password <span class="toggle-password" style="cursor:pointer"><i class="bi bi-eye-slash"></i></span>
-      <input name="confirmPass" type="password" required>
+      <input name="confirmPass" type="password" minlength="12" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{12,}$" required>
     </label>
     <button type="submit"><i class="bi bi-person-plus"></i> Signup</button>
   </form>
